afd38f13b016023bb4af09b4f0cb7aafb918fe86
mgoicochea <manuelgmdex@gmail.com>
2023-12-19 23:05:05 -0500

    se agrego ruta 404

--- src/app/domains/shared/pipes/time-ago.pipe.ts
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -1,4 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import{ formatDistance} from 'date-fns';
+
 
 @Pipe({
   name: 'timeAgo',
@@ -6,10 +8,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeAgoPipe implements PipeTransform {
 
-  transform(dateSent: string ): string {
+  /*transform(dateSent: string ): string {
     let currentDate = new Date();
     let dateSenta = new Date(dateSent);
-    return String(Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(dateSenta.getFullYear(), dateSenta.getMonth(), dateSenta.getDate()) ) /(1000 * 60 * 60 * 24))) ;
+  return String(Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(dateSenta.getFullYear(), dateSenta.getMonth(), dateSenta.getDate()) ) /(1000 * 60 * 60 * 24))) ;
+  }*/
+
+  transform(value: string ): string {
+    const date = new Date(value);
+    const today = new Date();
+    return formatDistance(today,date);
   }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
+  darkMode: ['class', '[data-mode="dark"]'],
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
